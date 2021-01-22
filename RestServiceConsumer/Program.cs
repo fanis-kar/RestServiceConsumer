851dc2622b16dfa53bb9bbe527a49e0697f6baa1
Theofanis Karamichalelis <fanis.karamichalelis@gmail.com>
2021-01-22 12:34:30 +0200

    Add project files.

---
+++ RestServiceConsumer/Program.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestServiceConsumer
+{
+    class Program
+    {
+        static void Main()
+        {
+            var jsonResult = GetData().Result;
+            var users = JsonConvert.DeserializeObject<List<Models.users>>(jsonResult);
+
+            Console.WriteLine(users[0].email);
+            Console.ReadKey();
+        }
+
+        static async Task<string> GetData()
+        {
+            var url = "https://jsonplaceholder.typicode.com/users";
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(url);
+                HttpResponseMessage response = await client.GetAsync(url);
+
+                if (!response.IsSuccessStatusCode)
+                    return null;
+
+                string strResult = await response.Content.ReadAsStringAsync();
+                return strResult;
+            }
+        }
+    }
+}
